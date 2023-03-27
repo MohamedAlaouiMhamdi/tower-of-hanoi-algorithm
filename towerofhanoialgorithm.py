9b14eed39f53959e156e798b9851a607da2804ac
Mohamed Alaoui Mhamdi <98537138+Otakoprogrammer@users.noreply.github.com>
2023-03-27 15:22:45 +0000

    Create towerofhanoialgorithm.py

---
+++ towerofhanoialgorithm.py
@@ -0,0 +1,12 @@
+def hanoi(n, from_rod, to_rod, aux_rod):
+    if n == 1:
+        print(f"Move disk 1 from {from_rod} to {to_rod}")
+        return
+    hanoi(n-1, from_rod, aux_rod, to_rod)
+    print(f"Move disk {n} from {from_rod} to {to_rod}")
+    hanoi(n-1, aux_rod, to_rod, from_rod)
+
+
+n = int(input("Enter the number of disks: "))
+input()
+hanoi(n, 'A', 'C', 'B')
